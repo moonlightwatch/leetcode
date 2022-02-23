583b41263b7944fff7d78a9801f2664c7cda7b9b
moonlightwatch <moonlightwatch@hotmail.com>
2022-02-23 17:37:21 +0800

    add 21

---
+++ leetcode/E_21.go
@@ -0,0 +1,72 @@
+package leetcode
+
+// https://leetcode-cn.com/problems/merge-two-sorted-lists/
+
+// 21. 合并两个有序链表
+
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+// mergeTwoLists 循环解
+func mergeTwoLists_(list1 *ListNode, list2 *ListNode) *ListNode {
+	// 执行用时：0 ms, 在所有 Go 提交中击败了100.00% 的用户
+	// 内存消耗：2.4 MB, 在所有 Go 提交中击败了99.98% 的用户
+
+	// 将 list2 按顺序插入到 list1, 返回list1。
+
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	node1 := list1
+	var frontNode *ListNode
+	for list2 != nil {
+		if node1 == nil {
+			frontNode.Next = list2
+			break
+		}
+		if node1.Val > list2.Val { // 需要将list2，插入到node1前面
+			next2 := list2.Next
+			if frontNode == nil {
+				list2.Next = node1
+				list1 = list2
+				node1 = list2.Next
+				frontNode = list2
+			} else {
+				frontNode.Next = list2
+				list2.Next = node1
+				frontNode = list2
+			}
+
+			list2 = next2
+		} else {
+			frontNode = node1
+			node1 = node1.Next
+		}
+	}
+	return list1
+}
+
+// mergeTwoLists 递归解
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
+
+func Test21() {
+	_printListNode(mergeTwoLists(_makeListNode([]int{1, 2, 4}), _makeListNode([]int{1, 3, 4}))) // [1,1,2,3,4,4]
+	_printListNode(mergeTwoLists(_makeListNode([]int{}), _makeListNode([]int{})))               // []
+	_printListNode(mergeTwoLists(_makeListNode([]int{}), _makeListNode([]int{0})))              // [0]
+}
